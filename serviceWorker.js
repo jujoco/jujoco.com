3b1877d3b0e8aca1bcf9870f0bc067831ea77fdc
Julian cobos <jujocotm@gmail.com>
2019-06-17 13:44:19 -0700

    disable service worker install

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,55 +12,52 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const CACHE_NAME = 'sw-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/assets/css/main.css',
-  '/assets/js/main.js'
-];
+// const CACHE_NAME = 'sw-cache-v1';
+// const urlsToCache = [
+//   '/',
+// ];
 
-self.addEventListener('install', (event) => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', (event) => {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then((cache) => {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
 
-        return fetch(event.request).then(
-          (response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+//         return fetch(event.request).then(
+//           (response) => {
+//             // Check if we received a valid response
+//             if (!response || response.status !== 200 || response.type !== 'basic') {
+//               return response;
+//             }
 
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
+//             // IMPORTANT: Clone the response. A response is a stream
+//             // and because we want the browser to consume the response
+//             // as well as the cache consuming the response, we need
+//             // to clone it so we have two streams.
+//             var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
+//             caches.open(CACHE_NAME)
+//               .then((cache) => {
+//                 cache.put(event.request, responseToCache);
+//               });
 
-            return response;
-          }
-        );
-      })
-  );
-});
+//             return response;
+//           }
+//         );
+//       })
+//   );
+// });
