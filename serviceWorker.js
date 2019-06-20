a79b24d0609e06cc86fbe665aa7c3c451e20a353
Julian cobos <jujocotm@gmail.com>
2019-06-19 18:56:37 -0700

    implement cache refresh

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,52 +12,58 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const CACHE_NAME = 'sw-cache-v2.0';
-const urlsToCache = [
-  '/index.html',
+const staticCacheName = 'sw-cache-v2.0';
+const filesToCache = [
+  '/assets/css',
+  'index.html',
 ];
 
-self.addEventListener('install', (event) => {
-  // Perform install steps
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
       })
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
+      .then(response => {
         if (response) {
+          console.log('Found ', event.request.url, ' in cache');
           return response;
         }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
 
-        return fetch(event.request).then(
-          (response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+        // TODO 4 - Add fetched files to the cache
 
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
+      }).catch(error => {
 
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
+        // TODO 6 - Respond with custom offline page
 
-            return response;
-          }
-        );
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
