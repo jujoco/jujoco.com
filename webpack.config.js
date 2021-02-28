fffbcd2abb6f92b11685cc61ed4c153126a35d8b
jujoco <jujocotm@gmail.com>
2021-02-28 01:40:48 -0800

    atomic commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,25 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './assets/src/index.js',
+  entry: './javascript/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: 'file-loader'
+        use: 'file-loader',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: 'file-loader'
-      }
-    ]
-  }
+        use: 'file-loader',
+      },
+    ],
+  },
 }
